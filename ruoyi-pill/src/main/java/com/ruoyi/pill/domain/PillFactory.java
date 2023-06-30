159fce5c4f4d949523596d7b1ab0f87329aee91c
xiong <2016674027@qq.com>
2023-06-30 17:29:55 +0800

    一、设计表结构

---
+++ ruoyi-pill/src/main/java/com/ruoyi/pill/domain/PillFactory.java
@@ -0,0 +1,11 @@
+package com.ruoyi.pill.domain;
+
+/**
+ * @ClassName PillFactory
+ * @Description TODO
+ * @Author JiangXiong
+ * @Date 2023/6/30 17:23
+ * @Version 1.0
+ */
+public class PillFactory {
+}
